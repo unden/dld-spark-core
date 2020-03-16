726622444928be8e8320a673aeb284cf399dcb28
zhangxinwei <linuxcast92@163.com>
2020-03-16 22:46:45 +0800

    commit

--- src/main/scala/com/duliday/spark/core/leetcode/array/FindDuplication.java
+++ src/main/scala/com/duliday/spark/core/leetcode/array/FindDuplication.java
@@ -34,10 +34,39 @@ public class FindDuplication {
         return -1;
     }
 
+    /**
+     * @param nums
+     * @return
+     * leetcode:287
+     * 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
+     * 将数组类比为一个链表，问题转换为求链表中环的入口
+     */
+    public int findDuplicate(int[] nums) {
+        // Find the intersection point of the two runners.
+        int tortoise = nums[0];
+        int hare = nums[0];
+        do {
+            tortoise = nums[tortoise];
+            hare = nums[nums[hare]];
+        } while (tortoise != hare);
+
+        // Find the "entrance" to the cycle.
+        int ptr1 = nums[0];
+        int ptr2 = tortoise;
+        while (ptr1 != ptr2) {
+            ptr1 = nums[ptr1];
+            ptr2 = nums[ptr2];
+        }
+
+        return ptr1;
+    }
+
     public static void main(String[] args) {
         FindDuplication findDuplication = new FindDuplication();
         int[] arr = {1, 2, 3, 3};
         System.out.println(findDuplication.getDuplicate(arr));
+        int[] arr2 = {1,1,1,1,1,1,1};
+        findDuplication.findDuplicate(arr2);
     }
 
 }
